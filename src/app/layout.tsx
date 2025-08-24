a7f1930135ebaced88e18038232195ffa5037729
adamsbear34 <es1230200306@gmail.com>
2025-08-24 20:23:14 +0300

    added tg setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Root } from "@/components/Root/Root";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Root>{children}</Root>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-
+import { Button, Section } from "@telegram-apps/telegram-ui";
 
 export default function HomePage() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-primary-500">
+      <Section
+          header="Features"
+          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
+        >
+          
+        </Section>
     </div>
   );
 }
